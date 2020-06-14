14285d500f4683ffafc21e83ce10373a07c11e52
Rishabh Thukral <38890245+rishabhthukral2@users.noreply.github.com>
2020-06-14 17:31:10 +0530

    Update mail_handler.php

--- mail_handler.php
+++ mail_handler.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 	if(isset($_POST['submit'])){
 		$name=$_POST['name'];
@@ -18,25 +17,4 @@
 			echo "Something went wrong!";
 		}
 	}
-=======
-<?php
-	if(isset($_POST['submit'])){
-		$name=$_POST['name'];
-		$email=$_POST['email'];
-		$phone=$_POST['phone'];
-		$msg=$_POST['msg'];
-
-		$to='rishabhthukral08@gmail.com'; // Receiver Email ID, Replace with your email ID
-		$subject='Form Submission';
-		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
-		$headers="From: ".$email;
-
-		if(mail($to, $subject, $message, $headers)){
-			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
-		}
-		else{
-			echo "Something went wrong!";
-		}
-	}
->>>>>>> 748d428a13b8be643f8b24d85235051ae95b9147
 ?>
